ac5b6e4621261931e462ba7c1acedbf6bb1052d2
Vishal Singh <133114520+vishal3432@users.noreply.github.com>
2026-03-31 22:10:19 +0530

    Update urls.py

--- Services/django_app/interactions/urls.py
+++ Services/django_app/interactions/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import InteractionView
+from .views import like_product
 
 urlpatterns = [
     path("interact/", InteractionView.as_view()),
+    path("like/", like_product),
 ]
