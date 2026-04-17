59ee3c462cd88c36c036192a82b1bc027aa810c8
Vishal Singh <133114520+vishal3432@users.noreply.github.com>
2026-04-18 01:03:24 +0530

    Update admin.py

--- Services/django_app/interactions/admin.py
+++ Services/django_app/interactions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Interaction
 
-# Register your models here.
+@admin.register(Interaction)
+class InteractionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'action', 'timestamp']
