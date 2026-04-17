352868232ad57c5a8715aed7f2266543bc155866
Vishal Singh <133114520+vishal3432@users.noreply.github.com>
2026-04-17 15:36:29 +0530

    Add SessionAuthentication to REST framework settings

--- Services/django_app/config/settings.py
+++ Services/django_app/config/settings.py
@@ -143,6 +143,7 @@ from datetime import timedelta
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
 }
 
