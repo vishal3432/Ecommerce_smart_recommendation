c80d6fa3af95988fdc9f6387269b997d2ddfcbee
Vishal Singh <133114520+vishal3432@users.noreply.github.com>
2026-04-17 16:08:56 +0530

    Update settings.py

--- Services/django_app/config/settings.py
+++ Services/django_app/config/settings.py
@@ -68,11 +68,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url  # pip install dj-database-url psycopg2-binary
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
